cdeb8742060bc696e2af0ae94a3dd3e5d6e8553e
김영인 <0inn1220@gmail.com>
2022-05-28 14:16:41 +0900

    [백준] 9934(완전 이진 트리)

---
+++ 트리/9934(완전 이진 트리).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int k, root;
+
+int main() {
+    cin >> k;
+    root = pow(2, k) / 2;
+    for (int i = 0, num; i < k; i++) {
+        cin >> num;
+    }
+    
+    return 0;
+}
