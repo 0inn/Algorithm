bde0a173595d9e985d4daff4456ba97f53814661
김영인 <0inn1220@gmail.com>
2022-05-30 14:00:44 +0900

    [백준] 9934(완전 이진 트리)

--- 트리/9934(완전 이진 트리).cpp
+++ 트리/9934(완전 이진 트리).cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 #include <cmath>
+#define MAX 1025
 using namespace std;
 
-int k, root;
+int k, input, root, cnt;
+int arr[MAX];
+bool check[MAX];
 
 int main() {
     cin >> k;
-    root = pow(2, k) / 2;
-    for (int i = 0, num; i < k; i++) {
-        cin >> num;
+    input = pow(2, k);
+    root = input / 2;
+    for (int i = 1; i < input; i++) {
+        cin >> arr[i];
+    }
+    cout << arr[root] << "\n";
+    check[root] = true;
+    
+    while(true) {
+        if (cnt == (k - 1)) break;
+        root /= 2;
+        for (int i = root; i < input; i+= root) {
+            if (check[i]) continue;
+            cout << arr[i] << " ";
+            check[i] = true;
+        }
+        cout << "\n";
+        cnt++;
     }
     
     return 0;
