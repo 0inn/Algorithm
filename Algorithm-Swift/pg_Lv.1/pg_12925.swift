2bdd02b5bdbced23c9da80ced9af710e2b8121bd
김영인 <0inn1220@gmail.com>
2022-09-28 22:56:45 +0900

    [pg] 12925 - 서울에서 김서방 찾기

---
+++ Algorithm-Swift/pg_Lv.1/pg_12925.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Algorithm-Swift
+//
+//  Created by 김영인 on 2022/09/27.
+//
+
+import Foundation
+
+func solution(_ s:String) -> Int {
+    return Int(s)!
+}
+
+print(solution("-1234"))
