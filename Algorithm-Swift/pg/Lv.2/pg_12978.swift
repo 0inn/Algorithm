4c87e5f0d468df7fbcd4cd49ddaff8340fe7b969
김영인 <0inn1220@gmail.com>
2022-10-27 12:49:44 +0900

    [pg] 12978 - 배달 (풀이 추가)

--- Algorithm-Swift/pg/Lv.2/pg_12978.swift
+++ Algorithm-Swift/pg/Lv.2/pg_12978.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-func solution(_ N:Int, _ road:[[Int]], _ k:Int) -> Int {
+func solution1(_ N:Int, _ road:[[Int]], _ k:Int) -> Int {
     var visit = Array(repeating: Int.max, count: N + 1)
     var cities = [Int: [[Int]]]()
     var queue = [[Int]]()
@@ -51,4 +51,59 @@ func solution(_ N:Int, _ road:[[Int]], _ k:Int) -> Int {
     }
     
     return ans
+}
+
+// 코드 개선
+func solution2(_ N:Int, _ road:[[Int]], _ k:Int) -> Int {
+    var dist = [Int](repeating: Int(1e9), count: N + 1)
+    var graph = [[(Int, Int)]](repeating: [], count: N + 1)
+    var q = [Int]()
+    
+    for r in road {
+        graph[r[0]].append((r[1], r[2]))
+        graph[r[1]].append((r[0], r[2]))
+    }
+    
+    // 다익스트라
+    q.append(1)
+    dist[1] = 0
+    
+    while !q.isEmpty {
+        let cur = q.first!
+        q.removeFirst()
+        
+        for (nxtN, nxtW) in graph[cur] {
+            let newWeight = dist[cur] + nxtW
+            if newWeight < dist[nxtN] && newWeight <= k {
+                q.append(nxtN)
+                dist[nxtN] = newWeight
+            }
+        }
+    }
+    
+    return dist.filter { $0 <= k }.count
+}
+
+// 플로이드 워셜
+func solution3(_ N:Int, _ road:[[Int]], _ k:Int) -> Int {
+    var dist = [[Int]](repeating: [Int](repeating: Int(1e9), count: N + 1), count: N + 1)
+
+    for i in 1...N {
+        dist[i][i] = 0
+    }
+    
+    for r in road {
+        dist[r[0]][r[1]] = min(dist[r[0]][r[1]], r[2])
+        dist[r[1]][r[0]] = min(dist[r[1]][r[0]], r[2])
+    }
+    
+    for k in 1...N {
+        for i in 1...N {
+            for j in 1...N {
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+            }
+        }
+    }
+    
+    return dist[1].filter { $0 <= k }.count
 }
