42e99841317a56cd20a5664783fc3457062d07c9
김영인 <0inn1220@gmail.com>
2022-11-02 10:52:25 +0900

    [algorithm] 소수 판별 함수 추가

--- Algorithm-Swift/algorithm/prime.swift
+++ Algorithm-Swift/algorithm/prime.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+// 에라토스테네스의 체
+
 var n = 100
 
 var primeArr = Array(repeating: 0, count: n + 1)
@@ -27,4 +29,36 @@ var x = 17
 
 if primeArr[x] != 0 {
     print("Prime")
+}
+
+
+// for문
+
+func isForPrime(_ n: Int) -> Bool {
+    if n == 1 { return false }
+    if n == 2 || n == 3 { return true }
+    
+    let sq = Int(sqrt(Double(n)))
+    
+    for i in 2...sq {
+        if n % i == 0 { return false }
+    }
+    
+    return true
+}
+
+
+// while문
+func isWhilePrime(_ n: Int) -> Bool {
+    if n == 1 { return false }
+    if n == 2 || n == 3 { return true }
+    
+    let sq = Int(sqrt(Double(n)))
+    
+    while i <= sq {
+        gaurd n % i != 0 else { return false }
+        i += 1
+    }
+    
+    return true
 }
