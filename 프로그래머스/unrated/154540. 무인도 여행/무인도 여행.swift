2fca21eb9c55aa46fff051c7c39ae3795a7364ac
Youngin Kim <74968390+0inn@users.noreply.github.com>
2023-04-03 18:45:13 +0900

    [unrated] Title: 무인도 여행, Time: 0.51 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.swift
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.swift
@@ -6,34 +6,42 @@ func solution(_ maps:[String]) -> [Int] {
     let M = maps[0].count
     var visited = [[Bool]](repeating: Array(repeating: false, count: M), count: N)
     var q: [(Int, Int)] = []
+    var dq: [(Int, Int)] = []
     let dx = [0, 0, -1, 1]
     let dy = [-1, 1, 0, 0]
     var days: Int = 0
     var ans: [Int] = []
     
+    func dequeue() ->(Int, Int) {
+        if dq.isEmpty {
+            dq = q.reversed()
+            q.removeAll()
+        }
+        return dq.popLast()!
+    }
+
     func bfs(_ x: Int, _ y: Int) {
         q.append((x, y))
         visited[x][y] = true
-        var front = 0
-        
-        while front < q.count {
-            var (curx, cury) = q[front]
-            front += 1
+
+        while !(q.isEmpty && dq.isEmpty) {
+            var (curx, cury) = dequeue()
             
             for dir in 0..<4 {
                 let nx = curx + dx[dir]
                 let ny = cury + dy[dir]
-                
+
                 if nx < 0 || ny < 0 || nx >= N || ny >= M { continue }
                 if visited[nx][ny] || maps[nx][ny] == "X" { continue }
-                
+
                 days += Int(maps[nx][ny])!
+                
                 visited[nx][ny] = true
                 q.append((nx, ny))
             }
         }
     }
-    
+
     for i in 0..<N {
         for j in 0..<M {
             if visited[i][j] || maps[i][j] == "X" { continue }
@@ -42,6 +50,6 @@ func solution(_ maps:[String]) -> [Int] {
             ans.append(days)
         }
     }
-    
+
     return ans.isEmpty ? [-1] : ans.sorted()
 }
