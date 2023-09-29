a405445d5edbdbbcb6e27a94dee352255d353b49
Youngin Kim <74968390+0inn@users.noreply.github.com>
2023-09-30 00:45:20 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
@@ -1,31 +1,38 @@
-#include <string>
 #include <vector>
 #include <iostream>
+#define MAX 1000001
 
 using namespace std;
 
-vector<int> solution(vector<int> sequence, int k) {
-    int s = 0, e = 0;
-    int sum = sequence[0];
-    int subLen = sequence.size() + 1;
-    pair<int, int> result;
-    
-    while (s <= e && e < sequence.size()) {
-        if (sum < k) sum += sequence[++e];
-        else if (sum == k) {
-            if (e-s+1 < subLen) {
-                subLen = e-s+1;
-                result = {s, e};
-            } else if (e-s+1 == subLen) {
-                if (s < result.first) {
-                    result = {s, e};
-                }
-            }
-            
-            sum -= sequence[s++];
-        } 
-        else sum -= sequence[s++];
+struct Answer {
+    int start, end, range;
+};
+
+Answer ans = Answer{0, 0, MAX};
+
+vector<int> solution(vector<int> seq, int k) {
+    seq.insert(seq.begin(), 0);
+    for(int i=1; i<seq.size(); i++) {
+        seq[i] += seq[i-1];
     }
     
-    return {result.first, result.second};
+    int start = 0, end = 1;
+    while(start <= end && end < seq.size()) {
+        int diff = (start == end) ? seq[start] : seq[end] - seq[start];
+        if(diff > k) {
+            start++;
+        } else {
+            if(diff == k) {
+                int range = end - start;
+                if(range < ans.range) {
+                    ans.start = start;
+                    ans.end = end;
+                    ans.range = range;
+                }
+            }
+            end++;
+        }
+    }
+    
+    return {ans.start, ans.end-1};
 }
