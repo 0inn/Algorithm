f37f0a1284254fbc07f7af195619575b050fea36
Youngin Kim <74968390+0inn@users.noreply.github.com>
2025-07-05 17:10:37 +0900

    Time: 0 ms (100%), Space: 19.2 MB (100%) - LeetHub

--- 0055-jump-game/0055-jump-game.swift
+++ 0055-jump-game/0055-jump-game.swift
@@ -1,24 +1,14 @@
 class Solution {
     func canJump(_ nums: [Int]) -> Bool {
-        var jump = Array(repeating: 0, count: nums.count)
+        var maxJump = 0
 
-        if nums.count <= 1 {
-            return true
-        }
-
-        for (index, num) in nums.enumerated() {
-            if (index != 0 && jump[index] != 1) || num == 0 {
-                continue
-            }
-            for j in 1...num {
-                let nxtIndex = index+j
-                if nxtIndex == nums.count-1 {
-                    return true
-                }
-                if (nxtIndex >= nums.count-1) {
-                    break;
-                }
-                jump[nxtIndex] = 1;
+        for (i, num) in nums.enumerated() {
+            if i > maxJump {
+                return false
+            } 
+            maxJump = max(maxJump, i+num)
+            if maxJump >= nums.count - 1 {
+                return true
             }
         }
 
